89b35704f25a3aaa00cf3e696d6a35d04d38bf9e
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-19 10:13:49 -0300

    Aula 6 - Estilizacao Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,85 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://portalbr.akamaized.net/brasil/uploads/2019/02/05164624/FBR-a-marca-criadora-dos-t%C3%AAnis-sem-calcanhar.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://portalbr.akamaized.net/brasil/uploads/2019/02/05164624/FBR-a-marca-criadora-dos-t%C3%AAnis-sem-calcanhar.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://portalbr.akamaized.net/brasil/uploads/2019/02/05164624/FBR-a-marca-criadora-dos-t%C3%AAnis-sem-calcanhar.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://portalbr.akamaized.net/brasil/uploads/2019/02/05164624/FBR-a-marca-criadora-dos-t%C3%AAnis-sem-calcanhar.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://portalbr.akamaized.net/brasil/uploads/2019/02/05164624/FBR-a-marca-criadora-dos-t%C3%AAnis-sem-calcanhar.jpg"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao Carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
