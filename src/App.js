cd36a74990210d4986614081f89de5f8676db910
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-19 12:01:38 -0300

    Aula 10 - Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
