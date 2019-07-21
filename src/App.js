6389a309d2d4a48cedc6aaed3735dd0aaa52d3f8
root <root@DESKTOP-77RQ0M5.localdomain>
2019-07-20 23:30:50 -0300

    Aula 26 - NavegandoSaga

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import Routes from './routes';
+import history from './services/history';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
 import store from './store';
@@ -12,12 +13,12 @@ import store from './store';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
